2ff0e73ff0e75baedebfb1cac4130414a89f744e
waleed967 <waleedqureshi90@yahoo.com>
2020-09-10 04:54:59 -0700

    Working one

---
+++ index.js
@@ -0,0 +1,58 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+nicknames = {};
+
+app.get('/', function(req, res) {
+    res.render('index.ejs', { root: 'views' });
+});
+
+
+io.on('connection', function(socket) {
+
+    //new user 
+    socket.on('new user', function(data, callback) {
+
+
+        if (nicknames.hasOwnProperty(data)) {
+            callback(false);
+        } else {
+            callback(true);
+            socket.nickname = data;
+            nicknames[socket.nickname] = { online: true };
+            console.log('user connected: ' + socket.nickname);
+            //  io.emit('update_personal', nicknames + ': Online');
+
+            updateNicknames();
+        }
+    });
+
+    // update all user name
+
+    function updateNicknames() {
+        io.sockets.emit('usernames', nicknames);
+
+    }
+
+    // send message 
+
+    socket.on('send message', function(data) {
+        console.log('message: ' + { msg: data, nick: socket.nickname });
+        io.sockets.emit('new message', { msg: data, nick: socket.nickname });
+    });
+
+    //disconnected service
+
+    socket.on('disconnect', function(data) {
+        console.log('user disconnected:' + socket.nickname)
+        if (!socket.nickname) return;
+        nicknames[socket.nickname].online = false;
+
+        updateNicknames();
+    });
+
+});
+
+http.listen(8087, function() {
+    console.log('listening on *:8087');
+});
