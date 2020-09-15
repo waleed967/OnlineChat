5dda90a1b02201a8c903df6b6807b63db5698ee3
waleed967 <waleedqureshi90@yahoo.com>
2020-09-14 23:59:41 -0700

    Encrypt&Decrypt

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const crypto = require('crypto');
+const algorithm = 'aes-256-cbc';
+const key = crypto.randomBytes(32);
+const iv = crypto.randomBytes(16);
+const { emit } = require('process');
+
+//var bcrypt = require('bcryptjs');
+
+//const saltRounds = 10;
+//const message = " ";
+//var hash = bcrypt.hashSync("B4c0/\/", salt);
+
 
 nicknames = {};
 
@@ -35,12 +47,80 @@ io.on('connection', function(socket) {
 
     }
 
+    socket.on('send message', function(data) {
+
+        io.sockets.emit('new message', { msg: data, nick: socket.nickname });
+
+        function encrypt(data) {
+            let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
+            let encrypted = cipher.update(data);
+            encrypted = Buffer.concat([encrypted, cipher.final()]);
+            return { iv: iv.toString('hex'), encryptedData: encrypted.toString('hex') };
+        }
+
+
+        function decrypt(data) {
+            let iv = Buffer.from(data.iv, 'hex');
+            let encrypteddata = Buffer.from(data.encryptedData, 'hex');
+            let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
+            let decrypted = decipher.update(encrypteddata);
+            decrypted = Buffer.concat([decrypted, decipher.final()]);
+            return decrypted.toString();
+        }
+        //bcrypt.genSalt(saltRounds, function(err, salt) {
+        //  if (err) {
+        // throw err
+        //  } else {
+        // bcrypt.hash(data, salt, function(err, encrypt) {
+        //  if (err) {
+        //   throw err
+        //  } else {
+        //console.log("encrypt data is" + data)
+        var gfg = encrypt(data);
+        console.log("encrypt data is: " + gfg.encryptedData);
+        console.log("decrypt data is: " + decrypt(gfg));
+        // console.log("encrypt data is" + data)
+
+
+        //}
+        // })
+        // }
+        // })
+        // console.log(data);
+    });
+
+
+
+
+
+
+
+
     // send message 
 
-    socket.on('send message', function(data) {
-        console.log('message: ' + { msg: data, nick: socket.nickname });
-        io.sockets.emit('new message', { msg: data, nick: socket.nickname });
-    });
+    //socket.on('send message', function(data) {
+    // io.sockets.emit('new message', { msg: data, nick: socket.nickname });
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //disconnected service
 
@@ -54,6 +134,6 @@ io.on('connection', function(socket) {
 
 });
 
-http.listen(8087, function() {
-    console.log('listening on *:8087');
+http.listen(80, function() {
+    console.log('listening on *:80');
 });
